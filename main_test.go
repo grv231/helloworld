2ec26ac4f3cf88e7d2b396ef880fb227ec4c278b
Gaurav Tripathi <gtripathi@researchnow.com>
2018-11-30 05:00:23 -0500

    Adding new entries

--- main_test.go
+++ main_test.go
@@ -2,8 +2,11 @@ package main
 
 import "testing"
 
-func TestHelloWorld(t *testing.T) {
-	if helloworld() != "Hello World!!" {
-		t.Fatal("Test fail")
+func TestHello(t *testing.T) {
+	got := Hello()
+	want := "Hello, world"
+
+	if got != want {
+		t.Errorf("got '%s' want '%s'", got, want)
 	}
 }
